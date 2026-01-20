1a518b876a881d46a996705231a92eed0dddeda3
Ayush Poudel <ayushpoudel@usf.edu>
2026-01-20 15:43:29 -0500

    Refactor cluster schema to streamline attributes and remove unused kubeconfig blocks

--- internal/schema/cluster_registry/cluster_schema.go
+++ internal/schema/cluster_registry/cluster_schema.go
@@ -2,59 +2,27 @@ package clusterSchema
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 )
 
 func ClusterAttributes() map[string]schema.Attribute {
 	return map[string]schema.Attribute{
 		"cluster_name":  schema.StringAttribute{Required: true},
-		"labels":        schema.MapAttribute{Optional: true, ElementType: types.StringType},
-		"source":        schema.StringAttribute{Optional: true},
+		"context":       schema.StringAttribute{Optional: true},
+		"source":        schema.StringAttribute{Computed: true},
 		"registered_at": schema.StringAttribute{Computed: true},
+		/*
+		 Author: @ayuspoudel
+
+		 kubeconfig contains resolved kubeconfig BYTES.
+		 Terraform is responsible for resolving file/env/s3.
+		 Sentinel treats this as opaque data.
+		*/
+		"kubeconfig": schema.StringAttribute{Required: true, Sensitive: true, PlanModifiers: []planmodifier.String{stringplanmodifier.UseStateForUnknown()}},
 	}
 }
 
 func ClusterBlocks() map[string]schema.Block {
-	return map[string]schema.Block{
-		"kubeconfig": kubeconfigBlock(),
-	}
-}
-
-func kubeconfigBlock() schema.Block {
-	return schema.SingleNestedBlock{
-		Blocks: map[string]schema.Block{
-			"file": kubeconfigfileBlock(),
-			"env":  kubeconfigEnvBlock(),
-			"s3":   kubeconfigS3Block(),
-		},
-	}
-}
-
-func kubeconfigfileBlock() schema.Block {
-	return schema.SingleNestedBlock{
-		Attributes: map[string]schema.Attribute{
-			"path":    schema.StringAttribute{Required: true},
-			"context": schema.StringAttribute{Optional: true},
-		},
-	}
-}
-
-func kubeconfigEnvBlock() schema.Block {
-	return schema.SingleNestedBlock{
-		Attributes: map[string]schema.Attribute{
-			"name":     schema.StringAttribute{Required: true},
-			"encoding": schema.StringAttribute{Optional: true},
-			"context":  schema.StringAttribute{Optional: true},
-		},
-	}
-}
-
-func kubeconfigS3Block() schema.Block {
-	return schema.SingleNestedBlock{
-		Attributes: map[string]schema.Attribute{
-			"bucket": schema.StringAttribute{Required: true},
-			"key":    schema.StringAttribute{Optional: true},
-			"region": schema.StringAttribute{Optional: true},
-		},
-	}
+	return map[string]schema.Block{}
 }
